09429624b95378cb46f2f9ee1a091be937bf1772
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-10-08 23:56:51 +0800

    Решила задачу 21

--- Program.cs
+++ Program.cs
@@ -2,4 +2,22 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 Console.Clear();
-Console.Write("Введите число:");
+double Dist(int x0, int y0, int z0, int x1, int y1, int z1)
+{
+double dist = Math.Sqrt(Math.Pow(x1 - x0, 2) + Math.Pow(y1 - y0, 2)+ Math.Pow(z1 - z0, 2));
+return dist;
+}
+Console.Write("Введите X0:");
+int Ax = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Y0:");
+int Ay = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Z0:");
+int Az = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите X1:");
+int Bx = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Y1:");
+int By = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Z1:");
+int Bz = int.Parse(Console.ReadLine()!);
+Console.Write($"Расстояние = {Dist(Ax, Ay, Az, Bx, By, Bz):f2}");
